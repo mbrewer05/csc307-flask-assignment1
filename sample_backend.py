177d3589517792fb7e1481534fa6b58e192f903b
Maxwell Brewer <44121859+mbrewer05@users.noreply.github.com>
2021-04-08 23:48:24 -0700

    part 4&5

--- sample_backend.py
+++ sample_backend.py
@@ -77,15 +77,23 @@ def get_users():
         resp = jsonify(success=True)
         return resp
     
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
-    if id :
+    if request.method == 'GET':
+        if id :
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return ({})
+        return users
+    elif request.method == 'DELETE':
+        resp = jsonify(success=True)
+        resp.status_code = 404
         for user in users['users_list']:
             if user['id'] == id:
-                return user
-        return ({})
-    return users
-    
+                users['users_list'].remove(user)
+                resp.status_code = 204
+        return resp
 
 def random_id():
     id = ''
